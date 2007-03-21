24630ceb0eaa92474141bbcad2dd23c11a9b4ee1
Luis Gracia <lgraval@gmail.com>
2007-03-21 16:33:39 +0000

    Updated make-distrib.sh

--- make-distrib.sh
+++ make-distrib.sh
@@ -9,8 +9,19 @@ tar=$plugin-v$version.tgz
 
 cd ../
 
-tar zcvf $tar $dir/pkgIndex.tcl $dir/contacts.tcl $dir/gui.tcl $dir/hbonds.tcl $dir/labels.tcl $dir/maingui.tcl $dir/rmsd.tcl $dir/object.tcl $dir/save.tcl $dir/utils.tcl $dir/README.txt
-
+tar zcvf $tar $dir/pkgIndex.tcl 
+$dir/README.txt
+$dir/maingui.tcl 
+$dir/rmsd.tcl 
+$dir/utils.tcl 
+$dir/object.tcl 
+$dir/save.tcl 
+$dir/contacts.tcl 
+$dir/hbonds.tcl 
+$dir/gui.tcl 
+$dir/labels.tcl 
+$dir/covar.tcl
+$dir/dist.tcl
 
 mv $tar $dir/versions
 chmod go+rX $dir/versions/$tar
